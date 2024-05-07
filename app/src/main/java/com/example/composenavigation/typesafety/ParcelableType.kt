78d2470cbd04b8f0ec8fa0556b2249a1f69ca62b
Edgar Miró Monleón <edgarmiro@gmail.com>
2024-05-07 05:59:00 +0200

    Pass Json as a method parameter

--- app/src/main/java/com/example/composenavigation/typesafety/ParcelableType.kt
+++ app/src/main/java/com/example/composenavigation/typesafety/ParcelableType.kt
@@ -9,6 +9,7 @@ import kotlinx.serialization.json.Json
 
 inline fun <reified T : Parcelable> parcelableType(
     isNullableAllowed: Boolean = false,
+    json: Json = Json,
 ) = object : NavType<T>(isNullableAllowed = isNullableAllowed) {
     override fun get(bundle: Bundle, key: String) =
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -18,24 +19,25 @@ inline fun <reified T : Parcelable> parcelableType(
             bundle.getParcelable(key)
         }
 
-    override fun parseValue(value: String): T = Json.decodeFromString(value)
+    override fun parseValue(value: String): T = json.decodeFromString(value)
 
-    override fun serializeAsValue(value: T): String = Json.encodeToString(value)
+    override fun serializeAsValue(value: T): String = json.encodeToString(value)
 
     override fun put(bundle: Bundle, key: String, value: T) = bundle.putParcelable(key, value)
 }
 
 inline fun <reified T : Any> serializableType(
     isNullableAllowed: Boolean = false,
+    json: Json = Json,
 ) = object : NavType<T>(isNullableAllowed = isNullableAllowed) {
     override fun get(bundle: Bundle, key: String) =
-        bundle.getString(key)?.let<String, T>(Json.Default::decodeFromString)
+        bundle.getString(key)?.let<String, T>(json::decodeFromString)
 
-    override fun parseValue(value: String): T = Json.decodeFromString(value)
+    override fun parseValue(value: String): T = json.decodeFromString(value)
 
-    override fun serializeAsValue(value: T): String = Json.encodeToString(value)
+    override fun serializeAsValue(value: T): String = json.encodeToString(value)
 
     override fun put(bundle: Bundle, key: String, value: T) {
-        bundle.putString(key, Json.encodeToString(value))
+        bundle.putString(key, json.encodeToString(value))
     }
 }
